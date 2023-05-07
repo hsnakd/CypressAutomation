e011a2c0c7d91117f0b9d525b8405b06d039928d
hsnakd <hsnakd10@gmail.com>
2023-05-07 23:47:47 +0300

    Day 4

--- cypress/e2e/testScructure.cy.js
+++ cypress/e2e/testScructure.cy.js
@@ -14,37 +14,19 @@ describe('Context: My First Tests', () => {
     afterEach(() => {
         // similar to afterMethod in TestNG
     })
-    it('Opening a web application', () => {
-        cy.visit('registration_form');
+    it.skip('Opening a web application', () => {
+        cy.visit('/registration_form');
     })
-
-    it('Test 2', () => {
-        expect(false).to.equal(false)
+    xit('Test 2', () => {
+        expect(false).to.equal(false);
     })
-
     it('Test 3', () => {
-        expect(false).not.to.equal(true)
+        expect(false).not.to.equal(true);
     })
-
     it('Test 4', () => {
-        expect(5).to.equal(5)
+        expect(5).to.equal(5);
     })
-
     it('Test 5', () => {
-        expect(true).to.equal('5' == 5)
+        expect(true).to.equal('5'==5);
     })
-
-/**
-    // skip
-        it.skip('Test 6', () => {
-            expect(true).to.equal('5' == 5)
-        })
-        
-    // only
-        it.only('Test 7', () => {
-            expect(true).to.equal('5' == 5)
-        })
-*/
-
-
 })
